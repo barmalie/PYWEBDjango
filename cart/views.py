4680d1b834983cf7b78ea4e6d218044e077eb9cc
russos <ro0ne@mail.ru>
2023-01-24 20:11:37 +0300

    commit

--- cart/views.py
+++ cart/views.py
@@ -3,6 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render, redirect
 from .models import Cart, CartItem
+# from django.views import View
+#
+# class CartView(View):
+#    def get(selfself, request):
+#       cart_items = CartItem.objects.filter(cart__user=request.user)
+#       total_price = sum(item.product.price * item.quantity for item in cart_items)
+#       context = {'cart_items': cart_items, 'total_price': total_price}
+#       return render(request, 'cart.html', context)
 
 
 def view_cart(request):
