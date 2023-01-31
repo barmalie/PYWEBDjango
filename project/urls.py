a16a842ddb1822b646e0a2b4a82b369960db6fc0
russos <ro0ne@mail.ru>
2023-01-31 18:50:06 +0300

    commit

--- project/urls.py
+++ project/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
    path('admin/', admin.site.urls),
-   path('cart/', include('cart.urls')),
-   path('', include('common.urls'))
+   path('cart/', include('apps.cart.urls')),
+   path('', include('apps.common.urls')),
+   path('', include('apps.test_app.urls')),
+   path('', include('apps.login.urls')),
 ]
