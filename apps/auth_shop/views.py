6d8dd5ab737dcef7afbc1f1b020598ce04f334da
russos <ro0ne@mail.ru>
2023-02-14 20:06:30 +0300

    п 28 отладчик

--- apps/auth_shop/views.py
+++ apps/auth_shop/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
+from apps.cart_shop.models import Cart
 
 class Login(View):
    def get(self, request):
@@ -31,7 +32,10 @@ class CreateUserView(View):
            email = form.cleaned_data.get('email')
            password = form.cleaned_data.get('password1')
            user = User.objects.create_user(username=username, email=email, password=password)
+           cart = Cart(user=user)
            user.save()
+           cart.save()
+           login(request, user)
            return redirect('home:index')
        return redirect('auth_shop:create')
        #return render(request, "auth_shop/create_account.html", context={'errors': form.errors})
