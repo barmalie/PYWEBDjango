38e80174c636d05de477b6f8b07bdec4506eafe0
russos <ro0ne@mail.ru>
2023-02-14 20:32:58 +0300

    п 43

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.views import View
 from apps.cart_shop.models import Product
-
+from apps.cart_shop.views import fill_card_in_session, fill_id_card_in_session
 
 class IndexShopView(View):
 
     def get(self, request):
+        fill_card_in_session(request)
+        fill_id_card_in_session(request)
+
         data = Product.objects.all()
         context = {'data': data
                    }
