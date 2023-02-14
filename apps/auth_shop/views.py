0e54cdd8315b8f26668317c503fc9e8e68839af5
russos <ro0ne@mail.ru>
2023-02-14 19:13:19 +0300

    практика 4 п 23 допилить как на скрине

--- apps/auth_shop/views.py
+++ apps/auth_shop/views.py
@@ -2,7 +2,7 @@ from django.views import View
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
-
+from .forms import CustomUserCreationForm
 
 class Login(View):
    def get(self, request):
@@ -19,6 +19,21 @@ class Login(View):
                print(user)
                return redirect('home:index')
        return redirect('auth_shop:login')
+
+class CreateUserView(View):
+   def get(self, request):
+       return render(request, "auth_shop/create_account.html")
+
+   def post(self, request):
+       form = CustomUserCreationForm(data=request.POST)
+       if form.is_valid():
+           username = form.cleaned_data.get('username')
+           email = form.cleaned_data.get('email')
+           password = form.cleaned_data.get('password1')
+           user = User.objects.create_user(username=username, email=email, password=password)
+           user.save()
+           return redirect('home:index')
+       return redirect('auth_shop:create')
 
 
 # class LoginView(View):
