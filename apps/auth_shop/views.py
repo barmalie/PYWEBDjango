ad5954d51e6f277428b76f76bf200e016aa62f1e
barmalie <6r0d5k1y@gmail.com>
2023-02-24 16:07:24 +0300

    связь с auth_shop

--- apps/auth_shop/views.py
+++ apps/auth_shop/views.py
@@ -2,7 +2,6 @@ from django.views import View
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
-from .forms import CustomUserCreationForm
 from apps.cart_shop.models import Cart
 
 class Login(View):
@@ -18,13 +17,12 @@ class Login(View):
            cart = Cart(user=user)
            if user is not None:
                login(request, user)
-               print(user)
                return redirect('home:index')
        return redirect('auth_shop:login')
 
 class CreateUserView(View):
    def get(self, request):
-       return render(request, "auth_shop/create_account.html")
+       return render(request, "auth_shop/index.html")
 
    def post(self, request):
        form = AuthenticationForm(data=request.POST)
@@ -41,11 +39,4 @@ class CreateUserView(View):
             login(request, user)
             return redirect('home:index')
        return redirect('auth_shop:login')
-       #return render(request, "auth_shop/create_account.html", context={'errors': form.errors})
 
-# class LoginView(View):
-#     def get(self, request):
-#        return render(request, 'login/index_login_2.html')
-#
-#     def post(self, request):
-#         return JsonResponse(request.POST, json_dumps_params={'indent': 4})
