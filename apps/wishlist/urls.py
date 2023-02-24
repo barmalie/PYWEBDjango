d25d0288621797d3fa4bb9efba65c8781e368a14
barmalie <6r0d5k1y@gmail.com>
2023-02-24 12:08:15 +0300

    wishlist:done

--- apps/wishlist/urls.py
+++ apps/wishlist/urls.py
@@ -1,2 +1,15 @@
-from django.db import
+from django.db import models
+from apps.cart.models import Cart
+from apps.cart_shop.models import Product
+
+class viewWishListItem(models.Model):
+   wishlist = models.ForeignKey(Cart, on_delete=models.CASCADE)
+   product = models.ForeignKey(Product, on_delete=models.CASCADE)
+   quantity = models.IntegerField(default=1)
+   created_at = models.DateTimeField(auto_now_add=True)
+   updated_at = models.DateTimeField(auto_now=True)
+
+   def __repr__(self):
+      return f"{self.wishlist}_{self.product}"
+
 ]
